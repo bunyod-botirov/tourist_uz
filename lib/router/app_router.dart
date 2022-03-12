2dd1114657ab462ed222dbe733ad6d6a029a16cc
AzizbekFlutterDeveloper <xxuliganov7@gmail.com>
2022-03-12 02:32:17 -0500

    flitter

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:tourist_uz/provider/bottom_bar_provider.dart';
 import 'package:tourist_uz/screens/home/bottom_bar_page.dart';
-import 'package:tourist_uz/screens/splash_page.dart';
-=======
 import 'package:tourist_uz/info_page/info_page.dart';
 import 'package:tourist_uz/screens/auth/splash_page.dart';
->>>>>>> 9286a348eb0af0485ba6afdf577742bb9c91ea04
+
 
 class AppRouter {
   static Route? onGenerateRoute(RouteSettings settings) {
